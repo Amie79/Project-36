763c490bc9d61b0fe40499dc52c805f8c20833d6
Amie79 <70701704+Amie79@users.noreply.github.com>
2020-11-18 00:01:45 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,50 @@
+class Food{
+
+    constructor(){
+        this.foodStock=0;
+        this.lastfed;
+        this.image=loadImage("images/Milk.png");
+    }
+
+    getFoodStock(){
+        return this.foodStock;
+    }
+    updateFoodStock(foodStock){
+
+        this.foodStock=foodStock;
+    }
+
+    deductFood(){
+        if(this.foodStock>0){
+        this.foodStock=this.foodStock-1;
+           }
+    }
+
+    display(){
+        var x=80,y=100;
+
+        imageMode(CENTER);
+        image(this.image,720,220,70,70);
+
+        if(this.foodStock!=0){
+            for(var i=0;i<this.foodStock;i++){
+                if(i%10==0){
+                    x=80;
+                    y=y+50;
+                }
+                
+            image(this.image,x,y,50,50);
+            x=x+30;
+            }
+            if(lastFed>=12){
+                text("Last Fed: "+lastFed%12 + "PM",350,30);
+              }
+              else if(lastFed==0){
+                text("Last Fed:12 AM",350,30);
+              }
+              else{
+                text("Last Feed: "+lastFed+"AM",350,30);
+              }
+            }
+        }
+    }
